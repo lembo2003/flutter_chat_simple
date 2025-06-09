16f8f647559e294e98dde3463f7bc2ee72ab77cb
Lembo - Tran Trung Dung <lembo0909@gmail.com>
2025-06-09 13:58:55 +0700

    feat: Implement login page UI
    This commit introduces the UI for the login page, including:
    
    - `MyButton` widget: A custom button component.
    - `MyTextField` widget: A custom text field component with styling for hint text, borders, and fill color.
    - Integration of `MyButton` and `MyTextField` into the `LoginPage`.
    - Basic layout with email and password fields, a login button, and a registration prompt.
    - `TextEditingController`s for email and password fields.
    - Placeholder `login` method.

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/components/my_button.dart';
+import 'package:flutter_chat/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  //email and pw textfield controller
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+  LoginPage({super.key});
+
+  //login method
+  void login() {}
 
   @override
   Widget build(BuildContext context) {
@@ -26,13 +36,43 @@ class LoginPage extends StatelessWidget {
                 color: Theme.of(context).colorScheme.primary,
               ),
             ),
+            SizedBox(height: 25),
             //Email textfield
-            TextField(),
+            MyTextField(
+              hintText: "Enter your email...",
+              obscureText: false,
+              controller: _emailController,
+            ),
+            SizedBox(height: 10),
             //Password textfield
-
+            MyTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _pwController,
+            ),
+            SizedBox(height: 25),
             //Login Button
-
+            MyButton(text: "Login", onTap: login),
+            SizedBox(height: 25),
             //Register
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Not a member? ",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Text(
+                  "Register now!",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
